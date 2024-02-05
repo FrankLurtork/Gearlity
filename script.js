ef61d93042d2c068bb902130d6cb6d9ee3decefc
Frank Lopez <frank_lopez_93@outlook.com>
2024-02-05 16:41:19 -0600

    Update3

---
+++ script.js
@@ -0,0 +1,15 @@
+function next1(){
+    window.location.href="#news1";
+}
+function next2(){
+    window.location.href="#news2";
+}
+function next3(){
+    window.location.href="#news3";
+}
+function next4(){
+    window.location.href="#news4";
+}
+function next5(){
+    window.location.href="#news5";
+}
